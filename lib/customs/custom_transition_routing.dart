095a099844c4a771ee11a07d38ea592c33dff217
CodingLegend27 <waffler.christoph@gmail.com>
2020-09-12 23:03:45 +0200

    new transition between pages

---
+++ lib/customs/custom_transition_routing.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CustomTransition extends PageRouteBuilder {
+  final Widget page;
+  CustomTransition({this.page})
+    : super(
+          pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+          ) =>
+              page,
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) =>
+              FadeTransition(
+                opacity: animation,
+                child: child,
+              ),
+        );
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
