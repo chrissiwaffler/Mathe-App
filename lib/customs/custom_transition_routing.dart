dab3afb8c975ac011b3f9b0b093019dbdb0dba4e
CodingLegend27 <waffler.christoph@gmail.com>
2020-10-26 23:11:00 +0100

    custom transition for splash screen

--- lib/customs/custom_transition_routing.dart
+++ lib/customs/custom_transition_routing.dart
@@ -4,29 +4,45 @@ class CustomTransition extends PageRouteBuilder {
   final Widget page;
   CustomTransition({this.page})
     : super(
-          pageBuilder: (
-            BuildContext context,
-            Animation<double> animation,
-            Animation<double> secondaryAnimation,
-          ) =>
-              page,
-          transitionsBuilder: (
-            BuildContext context,
-            Animation<double> animation,
-            Animation<double> secondaryAnimation,
-            Widget child,
-          ) =>
-              FadeTransition(
-                opacity: animation,
-                child: child,
-              ),
-        );
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      
+      pageBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Animation<double> secondaryAnimation,
+      ) =>
+          page,
+      transitionsBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Animation<double> secondaryAnimation,
+        Widget child,
+      ) =>
+          FadeTransition(
+            opacity: animation,
+            child: child,
+          ),
     );
-  }
+}
+
+class CustomTransitionSplashScreen extends PageRouteBuilder {
+  final Widget page;
+  CustomTransitionSplashScreen({this.page})
+    : super(
+      transitionDuration: Duration(seconds: 2),
+      pageBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Animation<double> secondaryAnimation,
+      ) =>
+          page,
+      transitionsBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Animation<double> secondaryAnimation,
+        Widget child,
+      ) =>
+          FadeTransition(
+            opacity: animation,
+            child: child,
+          ),
+    );
 }
